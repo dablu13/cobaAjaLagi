94a92a41bf40ad1c2668e8f788a251b9799401a6
dablu13 <32445500+dablu13@users.noreply.github.com>
2017-10-02 14:02:50 +0700

    Update segitigaForm.java
    Menambahkan kodingan utama

--- segitigaForm.java
+++ segitigaForm.java
@@ -1,3 +1,22 @@
 public class segitigaForm(){
-
+public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int x;
+        System.out.print("Masukin x   = ");
+        x = input.nextInt();
+        
+        System.out.println("-----------------");
+        System.out.println("Jika x =" + x);
+        System.out.println("Maka Hasilnya  ");
+        System.out.println("=====================");
+        
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("=====================");
+    }
 }
